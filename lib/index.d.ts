1506a162c5e35096923b6c63cba2bce633153a68
Damien Sawyer <damiensawyer@gmail.com>
2020-09-15 13:34:12 +0100

    comments

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,3 @@
-export declare function sum(a: number, b: number): number;
+/** Add two numbers together */
+export declare function sum(/**The first number*/ a: number, /**The second number*/ b: number): number;
 export declare const names = "Karsten and Damien";
